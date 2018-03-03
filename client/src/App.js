63b4f80a86366a9dd4353e423796396b9e0e7fb0
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-03-03 12:06:23 -0700

    added file structure and set  up backend dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-               
+        <h1>This is a sample Oswald</h1>       
+        <p>This is a sample raleway</p>
       </div>
     );
   }
