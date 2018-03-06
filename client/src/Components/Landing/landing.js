e307fbb53cd3ff4b6314935b62e526817dba0724
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-03-05 18:29:04 -0700

    added team pic, created routes file, imported HashRouter

--- client/src/Components/Landing/landing.js
+++ client/src/Components/Landing/landing.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
+import teamPic from '../../Assets/jvVarsity.jpg'
 
 export default class Landing extends Component {
     render() {
         return(
             <div className="container">
                 <div className="header oswald">
-                    <div className="align-left">Header</div>
+                    <p>Lehi Girls Basketball</p>
                 </div>
                 <div className="navbar oswald">Schedule</div>
-                <div className="image oswald">image</div>
+                <div className="image oswald">
+                <img src={teamPic} alt="team pic"/>
+                </div>
                 {/* <div className="mission">mission</div> */}
                 <div className="schedule oswald">schedule</div>
                 <div className="player oswald">player</div>
---
+++ client/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Landing from './Components/Landing/landing';
+import GameCard from './Components/Game Card/gameCard.js';
+import PlayerCard from './Components/Player Card/playerCard';
+import Results from './Components/Results/results';
+import Roster from './Components/Roster/roster';
+import Schedule from './Components/Schedule/schedule';
+import User from './Components/User/user';
+
+export default (
+    <Switch>
+        <Route component={Landing} exact path = '/' />
+        <Route component={GameCard} path = '/schedule/:id' />
+        <Route component={PlayerCard} path = '/roster/:id' />
+        <Route component={Results} path = '/results' />
+        <Route component={Roster} path = '/roster' />
+        <Route component={Schedule} path = '/schedule' />
+        <Route component={User} path = '/user' />        
+    </Switch>
+
+)
