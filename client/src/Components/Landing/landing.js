c9d99d45b72850d55235bcebcecf81229d18567b
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-03-07 20:52:47 -0700

    filled in more hard code for landing page

--- client/src/Components/Landing/landing.js
+++ client/src/Components/Landing/landing.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import teamPic from '../../Assets/jvVarsity.jpg'
+import Lehi from '../../Assets/lehi-logo.jpg';
+import Judge from '../../Assets/judge-memorial-logo.jpg';
+import Kaylie from '../../Assets/Kaylie.jpg';
 
 export default class Landing extends Component {
     render() {
@@ -25,8 +28,28 @@ export default class Landing extends Component {
                 <img src={teamPic} alt="team pic"/>
                 </div>
                 {/* <div className="mission">mission</div> */}
-                <div className="schedule oswald">schedule</div>
-                <div className="player oswald">player</div>
+                <div className="schedule oswald">
+                    <div>Upcoming Game</div>
+                    <div className='zRow'>
+                        <img src={Lehi} alt=""/>
+                        <p>@</p>
+                        <img src={Judge} alt=""/>
+                    </div>
+                    <div className="raleway yrow">
+                        <p>November 26, 2013 - 7:00pm</p>
+                        <p>Judge Memorial High School</p>
+                    </div>
+                </div>
+                <div className="player oswald">
+                    <div>Player of the Week</div>
+                    <div className="xRow raleway">
+                        <div className = "xImg"><img src={Kaylie} alt=""/></div>
+                        <div className="xCol">
+                            <p>#4 Kaylie Bartholomew</p>
+                            <p>"Kaylie is driven to be the best she can be and brings out the best in her teammates."</p>
+                        </div>
+                    </div>
+                </div>
                 <div className="social oswald">Follow Us</div>
                 <div className="coach oswald">
                     <p>Congrats on making it to State! We have worked hard to achieve this moment.</p>
