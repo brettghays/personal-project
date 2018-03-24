2230edeb379395367c8743269e0db4cbe7fec0fa
Brett Hays <brett.g.hays.coding@gmail.com>
2018-03-24 13:56:00 -0600

    protected user, newgame, newplayer

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import axios from 'axios';
 import Landing from './Components/Landing/landing';
 import GameCard from './Components/Game Card/gameCard.js';
 import PlayerCard from './Components/Player Card/playerCard';
@@ -10,6 +11,55 @@ import User from './Components/User/user';
 import NewGame from './Components/New Game/newGame';
 import NewPlayer from './Components/New Player/newPlayer';
 
+class CheckAuthentication extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            authenticated: false
+        }
+        this.checkAuthentication = this.checkAuthentication.bind(this);
+    }
+ 
+    componentDidMount() {
+        this.checkAuthentication();
+    }
+ 
+    checkAuthentication() {
+        axios.get('/api/auth/me')
+            .then(res => {
+                console.log('Authenticated? ', res);
+                if (!res.data) {
+                    this.setState({ loading: false, authenticated: false });
+                } else {
+                    this.setState({ loading: false, authenticated: true });
+                }
+            })
+    }
+ 
+    render() {
+        if (this.state.loading) {
+            return (
+                <div>
+                    Loading ...
+                </div>
+            )
+        }
+        //render ...loading if state.loading, props.children otherwise
+        if (this.state.authenticated) {
+            return (
+                <div>
+                    {this.props.children}
+                </div>
+            )
+        } else {
+            return (
+                <Redirect to='/' />
+            )
+        }
+    }
+ }
+
 export default (
     <Switch>
         
@@ -18,10 +68,16 @@ export default (
             <Route component={PlayerCard} path = '/roster/:id/:edit?' />
             <Route component={Results} path = '/results' />
             <Route component={Roster} path = '/roster' />
-            <Route component={Schedule} path = '/schedule' />
-            <Route component={User} path = '/user' /> 
-            <Route component={NewGame} path = '/edit/newGame' />
-            <Route component={NewPlayer} path = '/edit/newPlayer' />
+            <Route component={Schedule} path = '/schedule' /> 
+            <Route render={() => {
+                return(
+                    <CheckAuthentication>
+                        <Route component={User} path = '/user' />
+                        <Route component={NewGame} path = '/edit/newGame' />
+                        <Route component={NewPlayer} path = '/edit/newPlayer' />
+                    </CheckAuthentication>
+                )
+            }}></Route>
         
     </Switch>
 )
