45cc5c762baccf157f149e66da46a6853eccbe64
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-03-24 10:58:50 -0600

    updated landing page again to fix updateFirstname spelling line 10

--- client/src/Components/Landing/landing.js
+++ client/src/Components/Landing/landing.js
@@ -7,7 +7,7 @@ import Navbar from '../Navbar/navbar';
 import Lehi from '../../Assets/lehi-logo.jpg';
 import Judge from '../../Assets/judge-memorial-logo.jpg';
 import Kaylie from '../../Assets/Kaylie.jpg';
-import {updateSessionID, updateFirsname, updateLastname, updateEmail, updateIsCoach} from '../../Reducer/reducer';
+import {updateSessionID, updateFirstname, updateLastname, updateEmail, updateIsCoach} from '../../Reducer/reducer';
 
 class Landing extends Component {
     componentDidMount(){
